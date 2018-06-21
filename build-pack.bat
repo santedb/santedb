7e5c46026b7a338a43ba9cac4a85ef429ba73398
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-06-21 10:36:40 -0400

    Updated build script

--- build-pack.bat
+++ build-pack.bat
@@ -10,30 +10,31 @@ if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild
 	) else ( echo Unable to locate VS 2017 build tools, will use default build tools )
 )
 
-set cwd = %cd%
-echo Will use NUGET in %cd%
-echo Will use MSBUILD in %msbuild%
+if defined msbuild (
+	set cwd = %cd%
+	echo Will use NUGET in %cd%
+	echo Will use MSBUILD in %msbuild%
 
-FOR %%P IN (santedb-model,santedb-api,santedb-applets,santedb-bre-js,santedb-orm,santedb-cdss) DO (
-	echo Building %%P
-	pushd %%P
+	FOR %%P IN (santedb-model,santedb-api,santedb-applets,santedb-bre-js,santedb-orm,santedb-cdss) DO (
+		echo Building %%P
+		pushd %%P
 
-	FOR /R %%G IN (*.sln) DO (
-		echo Building %%~pG 
-		pushd %%~pG
-		%msbuild% %%G /t:restore /p:configuration=release /m
-		%msbuild% %%G /t:rebuild /p:configuration=release /m
+		FOR /R %%G IN (*.sln) DO (
+			echo Building %%~pG 
+			pushd %%~pG
+			%msbuild% %%G /t:restore /p:configuration=release /m
+			%msbuild% %%G /t:rebuild /p:configuration=release /m
+			popd
+		)
+
+		FOR /R %%G IN (*.nuspec) DO (
+			echo Packing %%~pG
+			pushd %%~pG
+			%cd%\nuget.exe restore -SolutionDirectory ..\
+			%cd%\nuget.exe pack -OutputDirectory %localappdata%\NugetStaging -prop Configuration=Release
+			popd
+		)	
 		popd
 	)
 
-	FOR /R %%G IN (*.nuspec) DO (
-		echo Packing %%~pG
-		pushd %%~pG
-		%cd%\nuget.exe restore -SolutionDirectory ..\
-		%cd%\nuget.exe pack -OutputDirectory %localappdata%\NugetStaging -prop Configuration=Release
-		popd
-	)	
-	popd
 )
-
-
