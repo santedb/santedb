0731e52e5ab71df10a4228b2326bd3f775bb4ed6
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-12-01 18:43:39 +0630

    Rev'd versions

---
+++ submodule-undo.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+	ECHO WILL PULL SUBMODULES
+	SET cwd = %cd%
+	FOR /D %%G IN (.\*) DO (
+		PUSHD %%G
+		IF EXIST ".git" (
+			ECHO Pulling %%G
+			git checkout master
+			git pull
+		)
+		POPD
+	)
