435cdbedda41cea2d3c1da2602f22f246fa0a9a9
Justin Fyfe <justin@fyfesoftware.ca>
2019-03-11 17:48:41 -0400

    Rev'd versions

--- build-nuget.bat
+++ build-nuget.bat
@@ -11,19 +11,22 @@ if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild
 )
 
 if defined msbuild (
-	set nuget="%cd%\.nuget\nuget.exe"
+	if exist "%cd%\.nuget\nuget.exe" (
+		set nuget="%cd%\.nuget\nuget.exe"
+	)
+	if defined nuget  (
 	echo Will use NUGET %nuget%
 	echo Will use MSBUILD in %msbuild%
 
-	FOR %%P IN (restsrvr, santedb-model,santedb-api,santedb-applets,santedb-bre-js,santedb-orm,santedb-cdss,santedb-client,reportr,santedb-restsvc,santedb-dc-core) DO (
+	if exist "%nuget%" (
+	FOR %%P IN (restsrvr, santedb-model,santedb-api,santedb-applets,santedb-bre-js,santedb-orm,santedb-cdss,santedb-restsvc,santedb-client,reportr,santedb-dc-core) DO (
 		echo Building %%P
 		pushd %%P
 
 		FOR /R %%G IN (*.sln) DO (
 			echo Building %%~pG 
 			pushd %%~pG
-			%msbuild% %%G /t:restore /p:configuration=release /m
-			%msbuild% %%G /t:rebuild /p:configuration=release /m
+			%msbuild% %%G /t:restore /t:rebuild /p:configuration=Release /m
 			popd
 		)
 
@@ -33,10 +36,19 @@ if defined msbuild (
 			if exist "packages.config" (
 				%nuget% restore -SolutionDirectory ..\
 			)
-			%nuget% pack -OutputDirectory %localappdata%\NugetStaging -prop Configuration=Release
+			if [%1] == [] (
+				%nuget% pack -OutputDirectory %localappdata%\NugetStaging -prop Configuration=Release 
+			) else (
+				echo Publishing NUPKG
+				%nuget% pack -prop Configuration=Release 
+				FOR /R %%F IN (*.nupkg) do (
+					%nuget% push %%F -Source https://api.nuget.org/v3/index.json -ApiKey %1
+				)
+			) 
 			popd
 		)	
 		popd
 	)
-
+	)
+)	
 )
