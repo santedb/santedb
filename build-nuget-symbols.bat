2850abfa3ab103eb55e9ddf2b0fb4c56b323384d
Justin Fyfe <justin@fyfesoftware.ca>
2019-12-12 08:10:44 -0500

    Updated build scripts

--- build-nuget-symbols.bat
+++ build-nuget-symbols.bat
@@ -2,16 +2,23 @@
 
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
         echo will use VS 2017 Enterprise build tools
-        set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+        set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin"
 ) else (
 	if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" (
         	echo will use VS 2017 Professional build tools
-	        set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
+	        set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin"
 	) else (
 		if exist "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" (
-	        echo will use VS 2017 Community build tools
-        	set msbuild="c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
-		) else ( echo Unable to locate VS 2017 build tools, will use default build tools )
+	        	echo will use VS 2017 Community build tools
+        		set msbuild="c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin"
+		) else ( 
+			if exist "c:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" (
+        			set msbuild="c:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin"
+	        		echo will use VS 2019 Pro build tools
+			) else (
+				echo Unable to locate VS 2017 or 2019 build tools, will use default build tools 
+			)
+		)
 	)
 )
 
@@ -32,7 +39,7 @@ if defined msbuild (
 			echo Entering %%~pG
 			pushd "%%~pG"
 			echo Now in %cd%
-			%msbuild% "%%G" /t:restore /t:rebuild /p:configuration=Debug  /m
+			%msbuild%\msbuild "%%G" /t:restore /t:rebuild /p:configuration=Debug  /m
 			popd
 		)
 
@@ -42,7 +49,7 @@ if defined msbuild (
 			if exist "packages.config" (
 				%nuget% restore -SolutionDirectory ..\
 			)
-			%nuget% pack -OutputDirectory "%localappdata%\NugetStaging" -prop Configuration=Debug -Symbols
+			%nuget% pack -OutputDirectory "%localappdata%\NugetStaging" -prop Configuration=Debug -Symbols -msbuildpath %msbuild%
 			popd
 		)	
 		popd
--- build-release.bat
+++ build-release.bat
@@ -1,19 +1,25 @@
 @echo off
 echo Will build and push NUGET
 
-
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
         echo will use VS 2017 Enterprise build tools
-        set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+        set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin"
 ) else (
 	if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" (
         	echo will use VS 2017 Professional build tools
-	        set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
+	        set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin"
 	) else (
 		if exist "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" (
-	        echo will use VS 2017 Community build tools
-        	set msbuild="c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
-		) else ( echo Unable to locate VS 2017 build tools, will use default build tools )
+	        	echo will use VS 2017 Community build tools
+        		set msbuild="c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin"
+		) else ( 
+			if exist "c:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" (
+        			set msbuild="c:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin"
+	        		echo will use VS 2019 Pro build tools
+			) else (
+				echo Unable to locate VS 2017 or 2019 build tools, will use default build tools 
+			)
+		)
 	)
 )
 
@@ -44,7 +50,7 @@ if defined msbuild (
 		FOR /R %%G IN (*.sln) DO (
 			echo Building %%~pG 
 			pushd "%%~pG"
-			%msbuild% "%%G" /t:restore /t:rebuild /p:configuration=Release /m
+			%msbuild%\msbuild "%%G" /t:restore /t:rebuild /p:configuration=Release /m
 			popd
 		)
 
@@ -52,15 +58,15 @@ if defined msbuild (
 			echo Packing %%~pG
 			pushd "%%~pG"
 			if exist "packages.config" (
-				%nuget% restore -SolutionDirectory ..\
+				%nuget% restore -SolutionDirectory ..\  -msbuildpath %msbuild%
 			)
 			if [%2] == [] (
-				%nuget% pack -OutputDirectory "%localappdata%\NugetStaging" -prop Configuration=Release 
+				%nuget% pack -OutputDirectory "%localappdata%\NugetStaging" -prop Configuration=Release -msbuildpath %msbuild%
 			) else (
 				echo Publishing NUPKG
 				%nuget% pack -prop Configuration=Release 
 				FOR /R %%F IN (*.nupkg) do (
-					%nuget% push "%%F" -Source https://api.nuget.org/v3/index.json -ApiKey %2
+					%nuget% push "%%F" -Source https://api.nuget.org/v3/index.json -ApiKey %2 
 				)
 			) 
 			popd
