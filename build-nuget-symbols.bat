190849fa03861324a3dc25a592af67f1d51fa570
Justin Fyfe <justin@fyfesoftware.ca>
2020-05-04 19:19:20 -0400

    Updated release batches

--- build-nuget-symbols.bat
+++ build-nuget-symbols.bat
@@ -1,63 +1,17 @@
-@echo off
-
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
-        echo will use VS 2017 Enterprise build tools
-        set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin"
-) else (
-	if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" (
-        	echo will use VS 2017 Professional build tools
-	        set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin"
-	) else (
-		if exist "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" (
-	        	echo will use VS 2017 Community build tools
-        		set msbuild="c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin"
-		) else ( 
-			if exist "c:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" (
-        			set msbuild="c:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin"
-	        		echo will use VS 2019 Pro build tools
-			) else (
-				echo Unable to locate VS 2017 or 2019 build tools, will use default build tools 
-			)
-		)
-	)
-)
-
-if defined msbuild (
-	if exist "%cd%\.nuget\nuget.exe" (
-		set nuget="%cd%\.nuget\nuget.exe"
-	)
-	if defined nuget  (
-	echo Will use NUGET %nuget%
-	echo Will use MSBUILD in %msbuild%
-
-	if exist "%nuget%" (
-	FOR %%P IN (restsrvr,santedb-model,santedb-api,santedb-applets,santedb-restsvc,santedb-bre-js,santedb-bis,santedb-orm,santedb-cdss,santedb-client,reportr,santedb-match,santedb-dc-core) DO (
-	if exist "%%P" (
+@ECHO OFF
+FOR %%P IN (restsrvr,santedb-model,santedb-api,santedb-applets,santedb-restsvc,santedb-bre-js,santedb-bis,santedb-orm,santedb-cdss,santedb-client,reportr,santedb-match,santedb-dc-core) DO (
+	IF EXIST "%%P" (
 		
-		echo Building %%P
-		pushd "%%P"
-
-		FOR %%G IN (*.sln) DO (
-			echo Entering %%~pG
-			pushd "%%~pG"
-			echo Now in %cd%
-			%msbuild%\msbuild "%%G" /t:restore /t:rebuild /p:configuration=Debug  /m
-			popd
+		ECHO Packaging %%P
+		PUSHD "%%P"
+		
+		FOR /R %%G IN (*.csproj) DO (
+			ECHO Entering %%~pG
+			PUSHD "%%~pG"
+			dotnet pack --output "%localappdata%\NugetStaging" --configuration DEBUG --include-symbols 
+			POPD
 		)
-
-		FOR /R %%G IN (*.nuspec) DO (
-			echo Packing %%~pG
-			pushd "%%~pG"
-			if exist "packages.config" (
-				%nuget% restore -SolutionDirectory ..\
-			)
-			%nuget% pack -OutputDirectory "%localappdata%\NugetStaging" -prop Configuration=Debug -Symbols -msbuildpath %msbuild%
-			popd
-		)	
-		popd
+		POPD
 
 	)
-	)
-)
-)	
 )
--- build-release.bat
+++ build-release.bat
@@ -1,82 +1,34 @@
 @echo off
 echo Will build and push NUGET
 
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
-        echo will use VS 2017 Enterprise build tools
-        set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin"
-) else (
-	if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" (
-        	echo will use VS 2017 Professional build tools
-	        set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin"
-	) else (
-		if exist "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" (
-	        	echo will use VS 2017 Community build tools
-        		set msbuild="c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin"
-		) else ( 
-			if exist "c:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" (
-        			set msbuild="c:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin"
-	        		echo will use VS 2019 Pro build tools
-			) else (
-				echo Unable to locate VS 2017 or 2019 build tools, will use default build tools 
-			)
-		)
-	)
-)
-
-rem delete existing packages 
-del *.nupkg /s /q
-
-if [%1] == [] (
-	echo Must specify version number
-	goto end
-)
-
-if defined msbuild (
-	echo "Will release %1"
-	if exist "%cd%\.nuget\nuget.exe" (
-		set nuget="%cd%\.nuget\nuget.exe"
-	)
-	if defined nuget  (
-	echo Will use NUGET %nuget%
-	echo Will use MSBUILD in %msbuild%
-
-	if exist "%nuget%" (
-	FOR %%P IN (restsrvr,santedb-model,santedb-api,santedb-applets,santedb-restsvc,santedb-bre-js,santedb-bis,santedb-orm,santedb-cdss,santedb-client,reportr,santedb-match,santedb-dc-core) DO (
-if exist "%%P" (
-		pushd "%%P"
-		echo Preparing assets : LICENSE, NOTICE, etc.
-		copy ..\LICENSE /y
-		copy ..\License.rtf /y
-		echo Building %%P
+FOR %%P IN (restsrvr,santedb-model,santedb-api,santedb-applets,santedb-restsvc,santedb-bre-js,santedb-bis,santedb-orm,santedb-cdss,santedb-client,reportr,santedb-match,santedb-dc-core) DO (
+	IF EXIST "%%P" (
+		PUSHD "%%P"
+		ECHO Preparing assets : LICENSE, NOTICE, etc.
+		COPY ..\LICENSE /y
+		COPY ..\License.rtf /y
+		ECHO Building %%P
 
 		IF EXIST ".git" (
 			git checkout master
 			git pull
 		)
 
-		FOR %%G IN (*.sln) DO (
-			echo Building %%~pG 
-			pushd "%%~pG"
-			%msbuild%\msbuild "%%G" /t:restore /t:rebuild /p:configuration=Release /m
-			popd
-		)
+		FOR /R %%G IN (*.csproj) DO (
+			ECHO Packing %%~pG
+			PUSHD "%%~pG"
 
-		FOR /R %%G IN (*.nuspec) DO (
-			echo Packing %%~pG
-			pushd "%%~pG"
-			if exist "packages.config" (
-				%nuget% restore -SolutionDirectory ..\  -msbuildpath %msbuild%
-			)
-			if [%2] == [] (
-				%nuget% pack -OutputDirectory "%localappdata%\NugetRelease" -prop Configuration=Release -msbuildpath %msbuild%
-			) else (
-				echo Publishing NUPKG
-				%nuget% pack -prop Configuration=Release 
-				FOR /R %%F IN (*.nupkg) do (
-					 %nuget% push "%%F" -Source https://api.nuget.org/v3/index.json -ApiKey %2 
+			IF [%2] == [] (
+				dotnet pack --output "%localappdata%\NugetRelease" --configuration Release 
+			) ELSE (
+				ECHO Publishing NUPKG
+				DEL bin\publish\*.nupkg /S /Q
+				dotnet pack --configuration Release --output "bin\publish" 
+				FOR /R %%F IN (*.nupkg) DO (
+					 dotnet nuget push "%%F" -s https://api.nuget.org/v3/index.json -k %2 
 				)
 			) 
-			popd
+			POPD
 		)
 
 		IF EXIST ".git" (
@@ -87,12 +39,6 @@ if exist "%%P" (
 			git push
 			git push --tags
 		)	
-		popd
-	)
+		POPD
 	)
 )
-)	
-)
-
-
-:end
