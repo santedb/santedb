058e65caf0900bee5b18f3d5544af1ed3d35db62
Justin Fyfe <justin@fyfesoftware.ca>
2020-06-07 15:40:01 -0400

    Updated to add logic to batch files to pull/push to/from branchs

--- build-release.bat
+++ build-release.bat
@@ -10,7 +10,6 @@ FOR %%P IN (restsrvr,santedb-model,santedb-api,santedb-applets,santedb-restsvc,s
 		ECHO Building %%P
 
 		IF EXIST ".git" (
-			git checkout master
 			git pull
 		)
 
@@ -35,6 +34,8 @@ FOR %%P IN (restsrvr,santedb-model,santedb-api,santedb-applets,santedb-restsvc,s
 			ECHO Pushing %%G
 			git add *
 			git commit -am "Release of version %1"
+			git checkout master
+			git merge develop
 			git tag -a v%1 -m "Release of version %1"
 			git push
 			git push --tags
