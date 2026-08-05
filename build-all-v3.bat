79b85eb302c1b03183ff351a87453351de099b7b
Justin Fyfe <justin@fyfesoftware.ca>
2026-08-05 12:23:10 -0400

    Updated the build-all to fix an else statement

--- build-all-v3.bat
+++ build-all-v3.bat
@@ -228,8 +228,7 @@ if [%msbuild%] == [] (
 	if exist "C:\Program Files\Microsoft Visual Studio\18\Professional\MSBuild\Current\Bin\MSBuild.exe" (
 	        	echo will use VS 2022 Pro build tools
         		set msbuild="C:\Program Files\Microsoft Visual Studio\18\Professional\MSBuild\Current\Bin"
-	)
-	else (
+	) else (
 		if exist "C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MsBuild.exe" (
 					echo will use VS 2022 Pro build tools
 					set msbuild="C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin"
